02e8792aad251857acb567c7e300c6d924e8dfed
aasavchenko <andrey.savchenko@tala.co>
2019-05-15 11:38:42 +0400

    1 lesson: add tank and movements

--- app/src/main/java/ru/appngo/tankstutorial/MainActivity.kt
+++ app/src/main/java/ru/appngo/tankstutorial/MainActivity.kt
@@ -2,10 +2,37 @@ package ru.appngo.tankstutorial
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.KeyEvent
+import android.view.KeyEvent.*
+import android.widget.FrameLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        when (keyCode) {
+            KEYCODE_DPAD_UP -> {
+                myTank.rotation = 0f
+                (myTank.layoutParams as FrameLayout.LayoutParams).topMargin += -50
+            }
+            KEYCODE_DPAD_LEFT -> {
+                myTank.rotation = 270f
+                (myTank.layoutParams as FrameLayout.LayoutParams).leftMargin += -50
+            }
+            KEYCODE_DPAD_DOWN -> {
+                myTank.rotation = 180f
+                (myTank.layoutParams as FrameLayout.LayoutParams).topMargin += 50
+            }
+            KEYCODE_DPAD_RIGHT -> {
+                myTank.rotation = 90f
+                (myTank.layoutParams as FrameLayout.LayoutParams).leftMargin += 50
+            }
+        }
+        myTank.requestLayout()
+        return super.onKeyDown(keyCode, event)
     }
 }
